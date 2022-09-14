287209cd86e3d299e14836f8889f0c9c74c7cf95
YoungHa Kim <ytok1108@aol.com>
2022-09-14 22:16:37 +0900

    190Static Mutexes__result: Mutex {     data: [         Log {             date: 2022-09-12             message: Everythings fine,         },         Log {             date: 2023-05-05,             message: Everything's fine         }     ]     poisoned: false,
    }

--- EngRust/190Static_Mutexes/staticmutexes/src/main.rs
+++ EngRust/190Static_Mutexes/staticmutexes/src/main.rs
@@ -1,3 +1,22 @@
+use std::sync::Mutex;
+
+#[derive(Debug)]
+struct Log {
+    date: &'static str,
+    message: String
+}
+
+static GLOBAL_LOGGER: Mutex<Vec<Log>> = Mutex::new(Vec::new());
+
+fn do_thing(date: &'static str) {
+    GLOBAL_LOGGER.lock().unwrap().push(Log {
+        date,
+        message: "Everything's fine".to_string()
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    do_thing("2022-09-12");
+    do_thing("2023-05-05");
+    println!("{GLOBAL_LOGGER:#?}");
 }
