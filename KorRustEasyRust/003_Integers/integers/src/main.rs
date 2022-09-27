9c673c390c71e50fb94cb8b0b4b2fc207f802bf0
YoungHa Kim <ytok1108@aol.com>
2022-09-27 21:47:54 +0900

    Kor003 integers

---
+++ KorRustEasyRust/003_Integers/integers/src/main.rs
@@ -0,0 +1,21 @@
+// https://github.com/Dhghomon/easy_rust/
+fn main() {
+    // i8, i16, i32, i64, i128, and isize.
+    // - minus sign
+    // signed Integer
+
+    // u8, u16, u32, u64, u128, and usize.
+    // + plus sign
+    // Unsigned
+
+    // bits
+    // 8 bit = 1byte
+
+    // u8 = 256
+
+    // isize
+    // usize
+    // 컴퓨터에 따른것 computer architecture
+    // 32bit 32비트 isize -> 32bit -> i32
+    // 64bit 64비트 isize -> 64bit -> i64
+}
