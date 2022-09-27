8d022e3e3390071d35a0147f0d17f93a291f4ab1
YoungHa Kim <ytok1108@aol.com>
2022-09-27 22:20:08 +0900

    chars___result:
    🦀
    굿:

---
+++ KorRustEasyRust/004char/chars01/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // println!("Hello, world Rust Lang");
+    let first_letter = 'A';
+    let space = ' '; // A space inside ' ' is also a char
+    println!("{space}");
+    println!("{space}");
+    println!("{space}");
+    let crab_face = '🦀';
+    println!("{crab_face}");
+    let other_language_char_han = '굿';
+    println!("{other_language_char_han}");
+}
