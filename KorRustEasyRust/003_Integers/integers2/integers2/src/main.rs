eba9760b62f13bbcdf0240b39118a856bca63223
YoungHa Kim <ytok1108@aol.com>
2022-09-27 21:52:01 +0900

    this arithmetic operation will overflow

---
+++ KorRustEasyRust/003_Integers/integers2/integers2/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let my_number = 100; // i32
+    let my_number2: u8 = 200;
+    let my_other_number = 200;
+    let third_number = my_number2 + my_other_number;
+    // type inference
+}
