5a98bd2e9912d8b102aee2b821ba188e5779eca9
YoungHa Kim <ytok1108@aol.com>
2022-11-26 23:44:24 +0900

    Vim easy rust test

---
+++ KorRustEasyRust/123Builderpattern/builder_pattern/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+struct Character {
+    name: String,
+    age: u8,
+    height: u32,
+    weight: u32,
+    lifestyte: LifeState,
+}
+
+#[derive(Debug)]
+enum LifeState {
+    Alive,
+    Dead,
+    NeverAlive,
+    Uncertain,
+}
+
+impl Default for Character {
+    fn default() -> Self {
+        Self {
+            name: "Billy".to_string(),
+            age: 15,
+            height: 170,
+            weight: 70,
+            lifestyte: LifeState::Alive,
+        }
+    }
+}
+
+fn main() {
+    let npc_1 = Character::default();
+    println!("{npc_1:?}")
+}
---
+++ KorRustEasyRust/126Test_driven_development2_calculator/testcalcu/src/main.rs
@@ -0,0 +1,24 @@
+fn math(input: &str) -> i32 {
+    9
+}
+// "1 + 1" -> 2
+
+#[cfg(test)]
+mod tests {
+    use super::math; // super = the space just above
+
+    #[test]
+    fn one_plus_one_is_two() {
+        assert_eq!(math("1 + 1"), 2);
+    }
+    #[test]
+    fn one_minus_two_is_minus_one() {
+        assert_eq!(math("1 - 2"), -1);
+    }
+    #[test]
+    fn one_minus_two_is_minus_two() {
+        assert_eq!(math("1 - -1"), 2);
+    }
+}
+
+fn main() {}
