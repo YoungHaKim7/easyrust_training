3ad06357f2af771531e10403c23c6d4f34b9ec64
YoungHa Kim <ytok1108@aol.com>
2022-12-25 20:26:38 +0900

    Kor 042__ More destructuring

---
+++ 04KorRustEasyRust/042_More_Destructuring/destructuring01/src/main.rs
@@ -0,0 +1,21 @@
+// destructuring
+
+struct Person {
+    name: String,
+    real_name: String,
+    height: u8,
+    happiness: bool,
+}
+
+
+fn main() {
+    let papa_doc = Person {
+        name: "Papa Doc".to_string(),
+        real_name: "Clarence".to_string(),
+        height: 170,
+        happiness: false
+    };
+
+    println!("They call hime {} but his real name is {}. He is {} cm tall and is he happy? {}",
+            papa_doc.name, papa_doc.real_name, papa_doc.height, papa_doc.happiness);
+}
