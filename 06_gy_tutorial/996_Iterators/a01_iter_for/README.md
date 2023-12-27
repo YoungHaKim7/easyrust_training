0c9c2290b2b1c551244d563b679d226b0fa7040f
YoungHa Kim <ytok1108@aol.com>
2023-12-27 21:15:13 +0900

    iter for

---
+++ 06_gy_tutorial/996_Iterators/a01_iter_for/README.md
@@ -0,0 +1,10 @@
+# Result
+
+```
+index: 1
+index: 2
+index: 34
+index: 5
+index: 6
+index: 8
+```
---
+++ 06_gy_tutorial/996_Iterators/a01_iter_for/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x_int = vec![1, 2, 34, 5, 6, 8];
+    for i in x_int.iter() {
+        println!("index: {}", i);
+    }
+}
