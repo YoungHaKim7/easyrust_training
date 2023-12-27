52c220b30195f1547809e47077e1a2ccbf01f4fa
YoungHa Kim <ytok1108@aol.com>
2023-12-27 21:00:21 +0900

    enumerus

---
+++ 06_gy_tutorial/996_Iterators/a01_enumerus_iter/README.md
@@ -0,0 +1,11 @@
+# Result
+
+```
+index0: 1
+index1: 2
+index2: 34
+index3: 5
+index4: 6
+index5: 8
+
+```
---
+++ 06_gy_tutorial/996_Iterators/a01_enumerus_iter/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x_int = vec![1, 2, 34, 5, 6, 8];
+    // let mut iter_x = x_int.iter();
+    for (i, element) in x_int.iter().enumerate() {
+        println!("index{}: {}", i, element);
+    }
+}
